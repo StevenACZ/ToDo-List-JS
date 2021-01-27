d4ee93a9b32ae595f1968ace8f0f8cbf389d49b3
stevenacz <scoailazaa@gmail.com>
2021-01-27 18:03:59 -0500

    desplazamiento finalizado

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,44 @@
 const btnToAddNewCategory = document.querySelector('.btn-to-add-new-category');
+const btnToAddNewTask = document.querySelector('.btn-to-add-new-task');
+
 const sectionAddNewTask = document.querySelector('.add-new-task');
 const sectionAddNewCategory = document.querySelector('.add-new-category');
 const sectionCategoryProjects = document.querySelector('.category-projects');
 
-const btnBackCategory = document.querySelector('.btn-back-category');
+const btnBackToMainMenuFromNewCategory = document.querySelector('.btn-back-to-main-menu-from-new-category');
+const btnBackToMainMenuFromCategoryProjects = document.querySelector('.btn-back-to-main-menu-from-category-projects');
+const btnBackToCategoryProjectsFromNewTask = document.querySelector('.btn-back-to-category-projects-from-new-task');
+
 const categoriesItem = document.querySelectorAll('.category__item');
 
 
+// OPEN BUTTONS
 btnToAddNewCategory.addEventListener('click', () => {
   sectionAddNewCategory.classList.add('section-active');
 });
 
-btnBackCategory.addEventListener('click', () => {
-  sectionAddNewCategory.classList.remove('section-active');
-})
+btnToAddNewTask.addEventListener('click', () => {
+  sectionAddNewTask.classList.add('section-active');
+});
 
 categoriesItem.forEach((categoryItem) => {
   categoryItem.addEventListener('click', () => {
     sectionCategoryProjects.classList.add('section-active');
+    btnToAddNewTask.style.zIndex = '20';
   })
 })
+
+// BACK BUTTONS
+btnBackToMainMenuFromNewCategory.addEventListener('click', () => {
+  sectionAddNewCategory.classList.remove('section-active');
+})
+
+btnBackToMainMenuFromCategoryProjects.addEventListener('click', () => {
+  sectionCategoryProjects.classList.remove('section-active');
+  btnToAddNewTask.style.zIndex = '-20';
+})
+
+btnBackToCategoryProjectsFromNewTask.addEventListener('click', () => {
+  sectionAddNewTask.classList.remove('section-active');
+})
+
