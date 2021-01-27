6aa856b89d3a402ec59e8327fa931866299de081
stevenacz <scoailazaa@gmail.com>
2021-01-27 17:15:38 -0500

    Estilos adicionales y mejoras en la estructuracion del programa

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,22 @@
-console.log("Hello, world");
+const btnToAddNewCategory = document.querySelector('.btn-to-add-new-category');
+const sectionAddNewTask = document.querySelector('.add-new-task');
+const sectionAddNewCategory = document.querySelector('.add-new-category');
+const sectionCategoryProjects = document.querySelector('.category-projects');
+
+const btnBackCategory = document.querySelector('.btn-back-category');
+const categoriesItem = document.querySelectorAll('.category__item');
+
+
+btnToAddNewCategory.addEventListener('click', () => {
+  sectionAddNewCategory.classList.add('section-active');
+});
+
+btnBackCategory.addEventListener('click', () => {
+  sectionAddNewCategory.classList.remove('section-active');
+})
+
+categoriesItem.forEach((categoryItem) => {
+  categoryItem.addEventListener('click', () => {
+    sectionCategoryProjects.classList.add('section-active');
+  })
+})
